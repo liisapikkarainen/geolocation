9edd5c96002364bbc4987b9b1a71e828e9f286ba
Liisa Pikkarainen <liisapikkarainen1@gmail.com>
2022-02-17 00:15:54 +0200

    sijainti koordinaatit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+        setIsLoading(false);
+      }, (error) => {
+        alert(error);
+      })
+    } else {
+      alert('öyöyöy!!!!!!!!!!!!');
+    }
+  }, [])
+
+  if (isLoading) {
+    return <div><p>Loading...</p></div>
+  } else {
+    return (
+      <div>
+        <p>Sijainti: {lat.toFixed(3)},{lng.toFixed(3)}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
+
+ 
 }
 
 export default App;
